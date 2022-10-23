e89e22eef276c7b140579fbd832630d8934240d5
jethro tang <jthtang10@gmail.com>
2022-10-23 23:19:54 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Component from "./Component";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
